e06553c2aeebb067a4f4fbadcadb6929f02a40cf
Waniek Anton-Nikolas <antonwnk@gmail.com>
2017-10-16 15:05:02 +0200

    Added favicon

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 admin.autodiscover()
 
 import hello.views
@@ -13,4 +15,5 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
 ]
